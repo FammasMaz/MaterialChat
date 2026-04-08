79cbafee64913d5e3d11f27b4c027ae849eae9d8
Moeeze Hassan <fammas.maz@gmail.com>
2026-04-09 00:22:13 +0200

    chore: revert manual version bump (release workflow handles versioning)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.materialchat"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "2.10.0"
+        versionCode = 68
+        versionName = "2.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
